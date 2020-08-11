2de49d2faca6c5471f97a492c806e6d29cd26c64
luc26 <38132002+luc26@users.noreply.github.com>
2020-08-11 12:59:10 -0300

    aula01 completa, aprendendo sobre servelets

---
+++ gerenciador/src/br/com/estudo/gerenciador/service/firstServelet.java
@@ -0,0 +1,27 @@
+package br.com.estudo.gerenciador.service;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns="/oi")
+public class firstServelet extends HttpServlet{
+
+	 @Override
+	    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	        
+		 	PrintWriter out = resp.getWriter();
+		 	out.println("<html>");
+		 	out.println("<body>");
+		 	out.println("Primeiro Servelet do arquivo FirstServelet");
+		 	out.println("</body>");
+		 	out.println("</html>");
+		 
+	    }
+
+}
