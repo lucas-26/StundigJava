f4c670eec69ea5cdba8da5bb6d56e9339c80fd71
luc26 <38132002+luc26@users.noreply.github.com>
2020-08-14 10:46:43 -0300

    aula30 finalizada

--- gerenciador/src/novaEmpresa/Banco.java
+++ gerenciador/src/novaEmpresa/Banco.java
@@ -1,10 +1,21 @@
 package novaEmpresa;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Banco {
 
-	public static List<Empresa> lista;
+	public static List<Empresa> lista = new ArrayList<>();
+	
+	static {
+		
+		Empresa empresa1 = new Empresa();
+		empresa1.setNome("Alura");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Caelum");
+		lista.add(empresa1);
+		lista.add(empresa2);
+	}
 	
 	public void adiciona(Empresa empresa) {
 		Banco.lista.add(empresa);
